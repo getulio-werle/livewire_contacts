38faf896071aabd77347a0fc04fc5cda6e3a5f2e
Getúlio Werle <getuliowerle10@gmail.com>
2025-04-08 08:45:09 -0400

    feat: initial layout and contacts migration

---
+++ resources/views/livewire/main-component.blade.php
@@ -0,0 +1,7 @@
+<div class="container my-5">
+    <div class="row">
+        <div class="col text-center">
+            <img src="{{ asset('assets/images/logo.png') }}" alt="logo" width="128px">
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire\MainComponent;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo env('APP_NAME');
-});
+Route::get('/', MainComponent::class);
